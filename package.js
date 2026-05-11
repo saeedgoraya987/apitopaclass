8263c31622fa38dfecfdd8ee4118da86dbbf0909
saeedgoraya987 <saeedgoraya987@gmail.com>
2026-05-11 13:14:32 +0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "stats-proxy-api",
+  "version": "1.0.0",
+  "description": "Proxy API for stats endpoint",
+  "main": "index.js",
+  "scripts": {
+    "start": "vercel dev"
+  },
+  "dependencies": {
+    "node-fetch": "^3.3.2"
+  }
+}
